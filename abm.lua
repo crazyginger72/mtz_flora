716e7785ffdd2906515cb8725305f061fee4ccbd
Craig Robbins <craigrobbins@users.sf.net>
2014-05-20 12:58:58 +1000

    Debugging

--- abm.lua
+++ abm.lua
@@ -1,9 +1,9 @@
 
 minetest.register_abm({
 	nodenames = {"group:flora"},
-	neighbors = {"default:dirt_with_grass", "default_jungletree", "default:dirt"},
+	neighbors = {"default:dirt_with_grass", "default_jungletree", "default:dirt", "mtz_flora:mycena"},
 	interval = 50,
-	chance = 25,
+	chance = 20,
 	action = function(pos, node)
 		pos.y = pos.y - 1
 		local under = minetest.get_node(pos)
@@ -11,12 +11,7 @@ minetest.register_abm({
 		if under.name ~= "default:dirt_with_grass" and under.name ~= "default:dirt" then
 			return
 		end
-		
-		local light = minetest.get_node_light(pos)
-		if not light or light < 13 then
-			return
-		end
-		
+				
 		local pos0 = {x=pos.x-4,y=pos.y-4,z=pos.z-4}
 		local pos1 = {x=pos.x+4,y=pos.y+4,z=pos.z+4}
 		if #minetest.find_nodes_in_area(pos0, pos1, "group:flora_block") > 0 then
@@ -39,7 +34,7 @@ minetest.register_abm({
 			--end
 			if minetest.get_node(seedling).name == "air" then
 				minetest.set_node(seedling, {name="mtz_flora:mycena"})
-				-- print("mycena spawned at ".. minetest.pos_to_string(pos))
+				print("mycena spawned at ".. minetest.pos_to_string(pos))
 			end
 		end
 	end,
